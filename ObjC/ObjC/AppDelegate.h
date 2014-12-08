08f0792931175b5c4f2096773188f67bdf569c29
Lars Tijsma <l.tijsma@gmail.com>
2014-12-08 09:34:19 +0100

    Initial commit

---
+++ ObjC/ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC
+//
+//  Created by Lars Tijsma on 08/12/14.
+//  Copyright (c) 2014 Lars Tijsma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjC/ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC
+//
+//  Created by Lars Tijsma on 08/12/14.
+//  Copyright (c) 2014 Lars Tijsma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
