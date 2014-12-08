30023ec913235b7d06acff928b583ba116b5b4f8
Lars Tijsma <l.tijsma@gmail.com>
2014-12-08 09:39:11 +0100

    weer een test commit

--- Swiftie/Swiftie/ViewController.swift
+++ Swiftie/Swiftie/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        log("hallo swiftie");
+        log("hallo s");
     }
 
     override func didReceiveMemoryWarning() {
